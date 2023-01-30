9ae9b9cf8d436512dce6c41f7eb813170a251bcc
JasubiPL <gameportal7@hotmail.com>
2023-01-30 13:22:12 -0600

    Se comienza Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import freecodecampLogo from './img/FreeCodeCamp_logo.png'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img src={freecodecampLogo} className='freecodecamp-logo' alt='Logo de FreeCodeCamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
